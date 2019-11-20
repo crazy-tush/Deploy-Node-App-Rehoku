5b7a8d9856906faf848e11274eb46bcc6579e71a
Tushar Borawake <tusharnandkum.boraw@PROD.HCLPNP.COM>
2019-11-20 15:32:42 +0530

    first checkin

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080, () => console.log('All is OK'))
+app.listen(process.env.PORT || 8080, () => console.log('All is Good'))
